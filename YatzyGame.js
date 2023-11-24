c407906d84e0e92e03c208201bd45346262cf439
ngob0002 <144380805+ngob0002@users.noreply.github.com>
2023-11-24 13:38:27 -0500

    Update YatzyGame.js

--- YatzyGame.js
+++ YatzyGame.js
@@ -1,146 +1,175 @@
-// YatzyGame.js
+
+const diceElements = document.querySelectorAll(".die");
+const diceObjects = []; //holds all dice objects
+const diceValues = []; //holds all values in the form of int
+diceElements.forEach((element, index) => {
+  diceObjects.push(new Dice((index + 1), element));
+});
+
 class YatzyGame {
-    constructor() {
-        this.currentRound = 1;
-        this.currentTrial = 1;
-        this.totalScore = 0;
-        this.totalRounds = 13;
-        this.numberOfDice = 5;
-        this.rolledDiceValues = [];
-        this.currentRoll = 0;
-        this.availableCombinations = [
-            'ones', 'twos', 'threes', 'fours', 'fives', 'sixes',
-            'threeOfAKind', 'fourOfAKind', 'fullHouse',
-            'smallStraight', 'largeStraight', 'chance', 'yatzy'
-        ];
-        this.turns = 0; // Reset the turns at the start of a new game.
-        this.rollButton = rollButton; // Store the roll button element.
-    
-    }
+  constructor() {
+    this.currentRound = 1;
+    this.totalScore = 0;
+    this.maxRounds = 13; // 13 rounds in Yatzy
+    this.maxTurnsPerRound = 3; // 3 turns per round
+    this.yatzyEngine = new YatzyEngine();
+    this.startNewGame();
+    //diceElements = document.querySelectorAll(".die");
+   
+    // Corrected definition using arrow function syntax
+    this.calculateAndSetScore = (category, index) => {
+      // Perform the calculation and set the score based on the selected category
+      //const diceValues = this.yatzyEngine.die.map(die => die.value);
+      const diceValue = diceValues[index];
 
-    startNewGame() {
-       
-        this.currentRound = 1;
-        this.currentTrial = 1;
-        this.totalScore = 0;
-        this.rolledDiceValues = [];
-        this.currentRoll = 0;
-        this.availableCombinations = [
-            'ones', 'twos', 'threes', 'fours', 'fives', 'sixes',
-            'threeOfAKind', 'fourOfAKind', 'fullHouse',
-            'smallStraight', 'largeStraight', 'chance', 'yatzy'
-        ];
-        this.turns = 0; // Reset the turns at the start of a new game.
-        this.rollButton.disabled = false; // Enable the roll button when starting a new game.
-    }
-    
-    rollDice() {
-        if (this.currentRoll >= 3) {
-            console.log('You have used up all your rolls for this turn.');
-            return;
-        }
+    this.yatzyEngine.calculateScore(category, diceValues);
+    };
 
-        this.rolledDiceValues = [];
-        for (let i = 0; i < this.numberOfDice; i++) {
-            this.rolledDiceValues.push(this.getRandomNumber(1, 6));
-        }
+    diceElements.forEach((die, index) => {
+      die.addEventListener('click', () => this.onDieClick(index));
+    });
 
-        this.currentRoll++;
-        this.turns++; // Increment the turns at each roll.
-        
-        if (this.turns >= 3) {
-            // Disable the roll button after three rolls in a round.
-            rollButton.disabled = true;
-        }
-        this.selectedDiceToReroll = [];
+    const rollButton = document.getElementById('roll-button');
+    const scoreButton = document.getElementById('score-button');
 
-    }
+    rollButton.addEventListener('click', () => this.onRollButtonClick());
+    scoreButton.addEventListener('click', () => this.onScoreButtonClick());
+  }
     
-    toggleSelectedDie(dieIndex) {
-        if (this.currentRoll >= 3) {
-            console.log('You have used up all your rolls for this turn.');
-            return;
-        }
-    
-        // Implement logic to select and hold dice for re-rolling.
-        if (this.selectedDiceToReroll.includes(dieIndex)) {
-            const index = this.selectedDiceToReroll.indexOf(dieIndex);
-            this.selectedDiceToReroll.splice(index, 1);
-        } else {
-            this.selectedDiceToReroll.push(dieIndex);
-        }
-    }
-    
+  startNewGame() {
+    this.currentRound = 1;
+    this.totalScore = 0;
+    this.yatzyEngine.resetScoreCard();
+    // Set the current round on the UI
+    document.getElementById('current-round').textContent = this.currentRound;
+  }
 
-    rerollSelectedDice() {
-        if (this.currentRoll >= 3) {
-            console.log('You have used up all your rolls for this turn.');
-            return;
-        }
-    
-        // Implement logic to re-roll selected dice.
-        for (const dieIndex of this.selectedDiceToReroll) {
-            this.rolledDiceValues[dieIndex] = this.getRandomNumber(1, 6);
-        }
-    
-        this.currentRoll++;
-        this.turns++; // Increment the turns at each roll.
-    }
-    
+  startNewRound() {
+    this.yatzyEngine.rollsLeft = 3;
+    this.yatzyEngine.selectedDice = [];
+    this.currentRound += 1;
+    document.getElementById('current-round').textContent = this.currentRound;
+  }
 
-    updateScore(score) {
-        this.totalScore += score;
-        if (this.currentTrial === 3) {
-            this.currentTrial = 1;
-            this.currentRound++;
-        } else {
-            this.currentTrial++;
-        }
+  endGame() {
+    // Display the final score for the single player
+    console.log(`Game over! Your final score: ${this.totalScore}`);
+    // You can also add any additional game-over logic or UI updates here.
+  }
+
+  endTurn() {
+      this.maxTurnsPerRound--;
+
+      // After the third roll, remove the click event listeners from dice elements to prevent further selection
+      if (this.maxTurnsPerRound === 0) {
+        diceElements.forEach((die) => {
+          die.removeEventListener('click', () => this.onDieClick(index));
+        });
+
+        // Start a new round immediately after three turns
+        this.startNewRound();
+      }
+
+      // Check if this was the last turn of the last round
+      if (this.maxTurnsPerRound <= 0 && this.currentRound >= this.maxRounds) {
+        this.endGame();
+      }
     }
 
 
-    selectCombinationFromScorecard(combination) {
-        if (this.currentRoll === 3) {
-            if (this.availableCombinations.includes(combination)) {
-                const score = yatzyEngine.calculateScore(combination, this.rolledDiceValues);
-                this.updateScore(score);
-                
-                // Update the score in the corresponding input field
-                const inputField = document.getElementById(combination);
-                inputField.value = score;
-    
-                const index = this.availableCombinations.indexOf(combination);
-                this.availableCombinations.splice(index, 1);
-                
-                // End the game after 13 rounds
-                if (this.currentRound > this.totalRounds) {
-                    this.endGame();
-                }
-            } else {
-                alert("Invalid combination selected.");
-            }
-        } else {
-            alert("You must complete three rolls before selecting a combination.");
-        }
-    }
-    
 
-    endGame() {
-        console.log(`Game Over! Your total score is ${this.totalScore}`);
-        const totalScoreElement = document.getElementById('total-score'); 
-        totalScoreElement.innerText = `Total Score: ${this.totalScore}`;
-    
-        // Show a game over message in an alert:
-        alert(`Game Over! Your total score is ${this.totalScore}`);
-    }
-    
 
-    getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+  onDieClick(index) {
+    diceElements[index].classList.toggle('selected');
+    //const dice = diceObjects[index].diceValue;
+
+    if (diceElements[index].classList.contains('selected')) {
+      this.yatzyEngine.selectDie(diceObjects[index]);
+    } else {
+      this.yatzyEngine.selectDie(diceObjects[index]);
     }
+  }
+
+ onRollButtonClick() {
+    this.yatzyEngine.rollDice();
+    this.updateDiceDisplay();
+  }
+
+  updateDiceDisplay() {
+  //const diceValues = this.yatzyEngine.die.map(die => die.diceValues[0]); // Update this line
+  diceElements.forEach((die, index) => {
+    // Check if the die is currently selected
+    const isSelected = die.classList.contains('selected');
+
+    // If the die is selected, don't change its background position during the animation
+    if (!isSelected) {
+      // Add a class to trigger the rolling animation
+      die.classList.add('rolling');
+      setTimeout(() => {
+        // After the rolling animation, update the background position
+        die.style.backgroundPosition = `${((diceValues[index] - 1) * -100)}px 0`;
+        // Remove the rolling class to stop the animation
+        die.classList.remove('rolling');
+      }, 1000); // Adjust the delay as needed
+    }
+  });
 }
 
-// Example usage:
-const rollButton = document.getElementById('roll-button'); // Get the roll button element
-const yatzyGame = new YatzyGame(rollButton);
-yatzyGame.startNewGame();
+
+  onScoreButtonClick() {
+  if (this.currentRound > this.maxRounds) {
+    this.endGame();
+  } else {
+    if (this.yatzyEngine.rollsLeft <= 0) {
+      const selectedCategory = document.getElementById('category-selector').value;
+
+      // Find the index of the selected die
+      let selectedIndex = -1;
+      diceElements.forEach((die, index) => {
+        if (die.classList.contains('selected')) {
+          selectedIndex = index;
+        }
+      });
+
+      if (selectedIndex !== -1) {
+        console.log('Calling calculateScore for category:', selectedCategory);
+
+        this.calculateAndSetScore(selectedCategory, selectedIndex);
+        this.updateScoreCardDisplay(); // Make sure this line is present
+        this.startNewRound();
+      } else {
+        console.log('Please select a die before scoring.');
+      }
+    } else {
+      console.log(`Round ${this.currentRound} completed. Rounds left: ${this.maxRounds - this.currentRound}`);
+    }
+  }
+}
+
+
+      
+  updateScoreCardDisplay() {
+  for (const category in this.yatzyEngine.scoreTable) {
+    const element = document.getElementById(category);
+    if (element) {
+      // Check if the element is an input field
+      if (element.tagName === 'INPUT') {
+        element.value = this.yatzyEngine.scoreTable[category] !== null
+          ? this.yatzyEngine.scoreTable[category]
+          : '';
+      } else {
+        // If it's not an input field, set the text content
+        element.textContent = this.yatzyEngine.scoreTable[category] !== null
+          ? this.yatzyEngine.scoreTable[category]
+          : '';
+      }
+    }
+  }
+}
+
+
+
+}
+document.addEventListener('DOMContentLoaded', function () {
+  const yatzyGame = new YatzyGame();
+});
