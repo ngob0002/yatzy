70b7be237947f7c9652d3d248937384abaa332b0
ngob0002 <144380805+ngob0002@users.noreply.github.com>
2023-11-24 13:39:13 -0500

    Update Dice.js

--- Dice.js
+++ Dice.js
@@ -1,18 +1,44 @@
-//Dice.js
 class Dice {
-    constructor(numberOfDice) {
-        this.numberOfDice = numberOfDice;
-        this.diceValues = new Array(numberOfDice).fill(1); // Initialize all dice to 1
-    }
+    constructor(numberOfDice, element) {
+      this.numberOfDice = numberOfDice;
+      this.diceValue = 1;//initialize all dice values to 1
+      this.element = element;
+    //this.diceValues = new Array(numberOfDice).fill(1); // Initialize all dice to 1
+  }
 
-    roll() {
-        this.diceValues = this.diceValues.map(() => this.getRandomNumber(1, 6));
-    }
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-    getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  getDiceValue (){
+    return this.diceValue;
+  }
+  
+
+  roll() {
+    this.startAnimation();
+
+    setTimeout(() => {
+      this.diceValue = this.getRandomNumber(1, 6);
+      console.log('Dice value after roll:', this.diceValue);
+      this.element.style.backgroundPosition = ((this.diceValue - 1) * -100) + "px 0";
+      this.stopAnimation();
+    }, 1000);
+  }
+    
+  startAnimation() {
+  const diceElement = document.getElementById(`die${this.numberOfDice}`);
+  if (diceElement) {
+    diceElement.classList.add('rolling');
+    console.log('Start rolling animation');
+  }
 }
 
-// Export the Dice class/module
-export default Dice;
+  stopAnimation() {
+  const diceElement = document.getElementById(`die${this.numberOfDice}`);
+  if (diceElement) {
+    diceElement.classList.remove('rolling');
+    console.log('Stop rolling animation');
+  }
+}
+}
